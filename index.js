55e834dfb551fae5af488fb8e91d94d9e23e436e
SharanjeyG <sharanjey023@gmail.com>
2023-07-15 16:19:47 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*Attendance*/
 document.addEventListener('DOMContentLoaded', function () {
 
     document.querySelector('.add').addEventListener('click', function() {
@@ -11,31 +12,66 @@ document.addEventListener('DOMContentLoaded', function () {
       document.querySelector('.popup-container').style.display = 'none';
     });
   
-    let today = document.getElementById("today");
+    /*let today = document.getElementById("today");
     let crtdte = new Date();
     let day = `${crtdte.getDate() < 10 ? "0" : ""}${crtdte.getDate()}`;
     let month = `${(crtdte.getMonth() + 1) < 10 ? "0" : ""}${crtdte.getMonth()+1}`;
     let yr = crtdte.getFullYear();
     console.log(month)
-    today.textContent =`${day}/${month}/${yr}`;
+    today.textContent =`${day}/${month}/${yr}`;*/
 
     const mark = document.querySelector('.work_but');
 
     mark.addEventListener('click',function(){
-      const male = parseInt(document.getElementById('male').value);
-      const fmale = parseInt(document.getElementById('female').value);
-      const cont = parseInt(document.getElementById('contract').value);
-      console.log(male);
+      let male = parseInt(document.getElementById('male').value);
+      let fmale = parseInt(document.getElementById('female').value);
+      let cont = parseInt(document.getElementById('contract').value);
+      /*console.log(male);
       console.log(fmale);
-      console.log(cont);
-      const count = male + fmale+ cont;
-      const store = document.querySelector('.count table tbody tr td');
-      store.textContent=count;
+      console.log(cont);*/
+      if(isNaN(male)){
+        male=0;
+      }
+      if(isNaN(fmale)){
+        fmale=0;
+      }
+      if(isNaN(cont)){
+        cont=0;
+      }
+      /*console.log(male);
+      console.log(fmale);
+      console.log(cont);*/
+      const ct_t = male + fmale+ cont;
+      const ct_p = male + fmale+ cont;
+      const tot = document.querySelector('.count table tbody tr .tot');
+      tot.textContent=ct_t;
+      const prs = document.querySelector('.count table tbody tr .prs');
+      prs.textContent=ct_p;
+      const abs = document.querySelector('.count table tbody tr .abs');
+      abs.textContent="0";
 
       document.querySelector('.workers form').reset();
     });
+
+    const present = document.getElementById('present');
+    const absent = document.getElementById('absent');
+    
+    present.addEventListener('click',function(){
+      document.getElementById('present').style.display = 'none';
+      document.getElementById('absent').style.display = 'none';
+      const dis = document.getElementById('dis');
+      dis.textContent="Present"
+    });
+
+    absent.addEventListener('click',function(){
+      document.getElementById('present').style.display = 'none';
+      document.getElementById('absent').style.display = 'none';
+      const dis = document.getElementById('dis');
+      dis.textContent="absent"
+    });
   });
   
+  /*Master Node*/
   document.addEventListener('DOMContentLoaded', function () {
     const ven = document.querySelector('.ven');
     const wor = document.querySelector('.wor');
@@ -53,11 +89,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     pro.addEventListener('click', function(){
       window.location.href = "prod_f.html";
-    });
-  
-    
+    }); 
   });
-  
+
+  /*Client Form*/
   document.addEventListener('DOMContentLoaded', function () {
     const save = document.querySelector('.cli_s');
     const clear = document.querySelector('.cli_c');
@@ -71,6 +106,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
   });
   
+  /*Product Form*/
   document.addEventListener('DOMContentLoaded', function () {
     const save = document.querySelector('.pro_s');
     const clear = document.querySelector('.pro_c');
@@ -84,6 +120,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
   });
   
+  /*Labour Form*/
   document.addEventListener('DOMContentLoaded', function () {
     const save = document.querySelector('.lab_s');
     const clear = document.querySelector('.lab_c');
@@ -97,6 +134,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
   });
   
+  /*Vendour Form*/
   document.addEventListener('DOMContentLoaded', function () {
     const save = document.querySelector('.ven_s');
     const clear = document.querySelector('.ven_c');
@@ -109,3 +147,18 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('form_v').reset();
     });
   });
+
+  /*Vendour Management*/
+  document.addEventListener('DOMContentLoaded', function () {
+    const pent = document.querySelector('.pent');
+    const pout = document.querySelector('.pout');
+  
+    pent.addEventListener('click', function(){
+      window.location.href = "pentrym.html";
+    });
+    pout.addEventListener('click', function(){
+      window.location.href = "pordm.html";
+    });
+  });
+
+  
